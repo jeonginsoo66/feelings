a05efcac160730c9f03b5b0a015750094df17e0b
jeonginsoo <macbookair13@jeong-insoo-Macbook-Air.local>
2020-09-05 02:26:38 +0900

    add album

--- routes/musics.js
+++ routes/musics.js
@@ -17,16 +17,16 @@ http://api.musixmatch.com/ws/1.1/artist.albums.get?artist_id=27561640&s_release_
 http://api.musixmatch.com/ws/1.1/track.search?q=bruno%20major&page_size=100&s_track_rating=desc
 
 - 한 개의 앨범 정보
-http://api.musixmatch.com/ws/1.1/album.get?album_id=26862948
+http://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=26862948&page_size=100
 
 - 한 개의 트랙 정보
 http://api.musixmatch.com/ws/1.1/track.get?commontrack_id=67804542
 */
 
-const musixmatchAPI = (apiMethod, param) => {
+const musixmatchAPI = (apiMethod, params) => {
   let apiUrl = apiBaseUrl + apiMethod + "?";
-  for (let key in param) {
-    apiUrl = apiUrl + key + "=" + param[key] + "&";
+  for (let key in params) {
+    apiUrl = apiUrl + key + "=" + params[key] + "&";
   }
 
   const apiResultPromise = new Promise((resolve, reject) => {
@@ -62,22 +62,36 @@ musicRouter.get("/search", (req, res) => {
 
   res.cookie("search", search, { maxAge: 60 * 60 * 24 * 1000 });
 
-  musixmatchAPI("track.search", {
-    q: search,
-    page_size: 100,
-    s_track_rating: "desc",
-    apikey,
-  })
-    .then((result) => {
-      if (isEmpty(result)) {
-        throw new Error();
+  Promise.all([
+    musixmatchAPI("track.search", {
+      q_lyrics: search,
+      page_size: 100,
+      s_track_rating: "desc",
+      apikey,
+    }),
+    musixmatchAPI("track.search", {
+      q_track_artist: search,
+      page_size: 100,
+      s_track_rating: "desc",
+      apikey,
+    }),
+  ])
+    .then(([lyricsResult, trackArtistResult]) => {
+      if (isEmpty(trackArtistResult.track_list)) {
+        res.render("musics/search", {
+          result: lyricsResult.track_list,
+          search,
+          see,
+          cookieSearch: req.cookies.search,
+        });
+      } else {
+        res.render("musics/search", {
+          result: trackArtistResult.track_list,
+          search,
+          see,
+          cookieSearch: req.cookies.search,
+        });
       }
-      res.render("musics/search", {
-        result: result.track_list,
-        search,
-        see,
-        cookieSearch: req.cookies.search,
-      });
     })
     .catch((err) => {
       console.log(err);
@@ -85,40 +99,59 @@ musicRouter.get("/search", (req, res) => {
     });
 });
 
-musicRouter.get("/lyrics/:commontrack_id", (req, res) => {
-  const { commontrack_id } = req.params;
+musicRouter.get("/lyrics/:commontrack_id/:album_id", (req, res) => {
+  const { commontrack_id, album_id } = req.params;
 
-  const apiGetPromise = musixmatchAPI("track.get", {
+  // 노래 제목, 아티스트명, 가사 가져오기
+  const getTrackPromise = musixmatchAPI("track.get", {
     commontrack_id,
     apikey,
   });
 
-  apiGetPromise.then((result) => {
-    if (isEmpty(result)) {
-      throw new Error();
-    }
-    let trackArr = result.track.track_share_url.split("/");
+  getTrackPromise
+    .then((result) => {
+      if (isEmpty(result)) {
+        throw new Error();
+      }
+      let trackArr = result.track.track_share_url.split("/");
 
-    let artist_name = trackArr[4];
-    let track_name = trackArr[5].split("?")[0];
+      let artist_name = trackArr[4];
+      let track_name = trackArr[5].split("?")[0];
 
-    const artistAndTitle = artist_name + "/" + track_name;
-    const trackPromise = scrappingAPI.getLyrics(artistAndTitle);
-    trackPromise
-      .then((track) => {
-        let lyricsData = track.lyrics.join().split("\n");
-        let albumSrc = "http:" + track.albumImg;
-        res.render("musics/lyrics", {
-          result,
-          albumSrc,
-          lyrics: lyricsData,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.redirect("/error");
+      const artistAndTitle = artist_name + "/" + track_name;
+      const trackPromise = scrappingAPI.getLyrics(artistAndTitle);
+
+      // 해당 트랙의 앨범 정보
+      const getAlbumPromise = musixmatchAPI("album.tracks.get", {
+        album_id,
+        page_size: 100,
+        apikey,
       });
-  });
+
+      Promise.all([trackPromise, getAlbumPromise])
+        .then(([track, albums]) => {
+          if (isEmpty(track) || isEmpty(albums)) {
+            throw new Error();
+          }
+          let lyricsData = track.lyrics.join().split("\n");
+          let albumSrc = "http:" + track.albumImg;
+
+          res.render("musics/lyrics", {
+            result,
+            albumSrc,
+            lyrics: lyricsData,
+            albums: albums.track_list,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/error");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/error");
+    });
 });
 
 module.exports = musicRouter;
